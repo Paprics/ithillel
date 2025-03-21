f86b68596fd3045557f29cfcc2c3fdba580ffc52
Valeriy <paprics@gmail.com>
2025-03-21 10:18:26 +0200

    Added cached func

--- HW1/main.py
+++ HW1/main.py
@@ -1,5 +1,7 @@
 import sys
+from collections import OrderedDict
 from functools import wraps
+
 
 def get_size(func):
     @wraps(func)
@@ -11,3 +13,31 @@ def get_size(func):
 
     return wrapper
 
+
+def lfu_cache(max_limit=64):
+    def cache_decorator(func):
+        @wraps(func)
+        def decorator(*args, **kwargs):
+            cache_key = (args, tuple(kwargs.items()))
+
+            if cache_key in decorator._cache:
+                decorator._total[cache_key] += 1
+                return decorator._cache[cache_key]
+
+            result = func(*args, **kwargs)
+
+            if len(decorator._cache) >= max_limit:
+                lfu_key = min(decorator._total, key=decorator._total.get)
+                del decorator._cache[lfu_key]
+                del decorator._total[lfu_key]
+
+            decorator._cache[cache_key] = result
+            decorator._total[cache_key] = 1
+
+            return result
+
+        decorator._cache = OrderedDict()
+        decorator._total = {}
+        return decorator
+
+    return cache_decorator
