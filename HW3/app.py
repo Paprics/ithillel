bb983e141c43337f866dc41d47738fcd8643520e
Valeriy <paprics@gmail.com>
2025-03-24 17:14:00 +0200

    Fix зÐcode

--- HW3/app.py
+++ HW3/app.py
@@ -1,41 +1,32 @@
-from flask import Flask, render_template_string, request
+from flask import Flask, render_template_string
 from faker import Faker
 import csv
-import requests as rqvsts
+import requests
 from http import HTTPStatus
 from webargs import fields, validate
-from webargs.flaskparser import use_args
+from webargs.flaskparser import use_kwargs
 
 app = Flask(__name__)
 faker = Faker()
 
-# dictionary validate parameters
-user_args = {
-    'quality': fields.Int(validate=validate.Range(min=1, max=1000), load_default=1),
-    'currency': fields.Str(validate=validate.Length(min=3), load_default='USD'),
-    'convert': fields.Int(validate=validate.Range(min=1), load_default=1)
-}
-
 
 @app.route('/generate_students')
-@use_args(user_args, location='query')  # accepts validated parameters dictionary, location
-def generate_students(args):
-
-    # http://127.0.0.1:5000/generate_students?quality=100
+@use_kwargs({
+    'quality': fields.Int(validate=validate.Range(min=1, max=1000), load_default=1)},
+    location='query')
+def generate_students(quality):
 
     list_students = []
 
-    # Generate list of students
-    for _ in range(int(args['quality'])):
-        first_name = faker.first_name()
-        last_name = faker.last_name()
-        email = faker.email(domain='gmail.com')
-        password = faker.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)
-        birthday = faker.date_of_birth(minimum_age=18, maximum_age=60)
-        student = [first_name, last_name, email, password, birthday]
+    for _ in range(quality):
+        student = [
+            faker.first_name(),
+            faker.last_name(),
+            faker.email(domain='gmail.com'),
+            faker.password(length=10, special_chars=True, digits=True, upper_case=True, lower_case=True),
+            faker.date_of_birth(minimum_age=18, maximum_age=60)]
         list_students.append(student)
 
-    # Save list of students to CSV file
     with open('students.csv', 'w', newline='') as file:
         headers = ['first_name', 'last_name', 'email', 'password', 'birthday']
         writer = csv.writer(file)
@@ -54,22 +45,20 @@ def generate_students(args):
 
 
 @app.route('/bitcoin_rate')
-@use_args(user_args, location='query')
-def bitcoin_rate(args):
-
-    # http://127.0.0.1:5000/bitcoin_rate?currency=UAH&convert=100
-
-    currency = args['currency']
-    convert = args['convert']
+@use_kwargs({
+    'currency': fields.Str(validate=validate.Length(min=3), load_default='USD'),
+    'convert': fields.Int(validate=validate.Range(min=1), load_default=1)},
+    location='query')
+def bitcoin_rate(currency, convert):
 
     url = f"https://bitpay.com/rates/BTC/{currency}"
-    response = rqvsts.get(url)
+    response = requests.get(url)
 
     if response.status_code != HTTPStatus.OK:
         return '<h1>Ошибка запроса!</h1>'
 
-    rate = response.json()['data']['rate']
-    name = response.json()['data']['name']
+    rate = response.json().get('data', {}).get('rate')
+    name = response.json().get('data', {}).get('name')
 
     return (f"<h1>Обменять - {convert} {name} на Bitcoin</h1><br>"
             f"<h2>Курс - {rate} {currency}</h2><br>"
@@ -78,6 +67,7 @@ def bitcoin_rate(args):
 
 @app.route('/')
 def home_page():
+
     return """
         <h1><a href="http://127.0.0.1:5000/generate_students?quality=100" target="_blank">Сгенерировать студентов</a><br>
         <a href="http://127.0.0.1:5000/bitcoin_rate?currency=UAH&convert=100" target="_blank">Обмен валют</a></h1>
